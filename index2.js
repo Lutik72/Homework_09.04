1e130a05d324f0faf029c1b780582b2ddb9b37be
Lutik72 <gribova.sofia24@yandex.ru>
2025-04-09 20:36:57 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,11 @@
+// Запрос числа у пользователя
+let number = Number(prompt("Введите число:"));
+
+// Условный оператор
+if (number > 5) {
+    console.log("Число больше 5");
+} else if (number < 5) {
+    console.log("Число меньше 5");
+} else {
+    console.log("Число равно 5");
+}
